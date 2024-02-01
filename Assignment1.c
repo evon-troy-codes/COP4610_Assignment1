ae269978587a1c02c0842e8c5569781e49490b22
Evon Troy Alexander <evont.alexander@gmail.com>
2024-02-01 18:14:07 -0500

    Revised function definitions and added comments.

--- Assignment1.c
+++ Assignment1.c
@@ -17,10 +17,10 @@ int main()
     printf("How many child processes would you like to create? Please enter a number up to 5: ");
     scanf("%d", &num);
 
-    // If the number is > 5, we'll prompt the user to enter a valid number
-    while (num > 5)
+    // If the number is > 0 AND < 5, we'll prompt the user to enter a valid number
+    while (num < 1 || num > 5)
     {
-        printf("Invalid entry. Child processes cannot exceed 5: ");
+        printf("Invalid entry. Child processes should be in the range of 1-5: ");
         scanf("%d", &num);
     }
     // User is notified of the number of child processes to be created
@@ -62,82 +62,84 @@ int main()
 
     return 0;
 
-    //         // Function to determine the sum of the factors of a number
-    // int factorial(int sum_factor)
-    // {
-    //     int i, sum = 0;
-    //     for (i = 1; i <= sum_factor; i++)
-    //     {
-    //         if (sum_factor % i == 0)
-    //         {
-    //             sum += i;
-    //         }
-    //     }
-    //     return sum;
-    // }
+    // Function definitions
 
-    // // Function to determine if a number is prime
-    // int is_prime_num(int num)
-    // {
-    //     // In the initial loop we'll check to see if the number is 1 or 2
-    //     if (num < 2)
-    //     {
-    //         return 0; // Not a prime number
-    //     }
-    //     for (int i = 2; i < num; i++)
-    //     {
-    //         if (num % i == 0)
-    //         {
-    //             return 0; // Not a prime number
-    //         }
-    //     }
-    //     return 1; // Prime number
-    // }
+    // Function to calculate the factorial of a number
+    int factorial(int sum_factor)
+    {
+        int i, sum = 0;
+        for (i = 1; i <= sum_factor; i++)
+        {
+            if (sum_factor % i == 0)
+            {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    // Function to determine if a number is prime
+    int is_prime_num(int num)
+    {
+        // In the initial loop we'll check to see if the number is 1 or 2
+        if (num < 2)
+        {
+            return 0; // Not a prime number
+        }
+        for (int i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                return 0; // Not a prime number
+            }
+        }
+        return 1; // Prime number
+    }
 
     // Function to determine if palindrome
-    // int is_palindrome(int num)
-    // {
-    //     int reversed_num = 0, remainder, original_num;
-    //     original_num = num;
-    //     while (num != 0)
-    //     {
-    //         remainder = num % 10;
-    //         reversed_num = reversed_num * 10 + remainder;
-    //         num /= 10;
-    //     }
-    //     if (original_num == reversed_num)
-    //     {
-    //         return 1; // Palindrome
-    //     }
-    //     else
-    //     {
-    //         return 0; // Not a palindrome
-    //     }
-    // }
+    int is_palindrome(int num)
+    {
+        int reversed_num = 0, remainder, original_num;
+        original_num = num;
+        while (num != 0)
+        {
+            remainder = num % 10;
+            reversed_num = reversed_num * 10 + remainder;
+            num /= 10;
+        }
+        if (original_num == reversed_num)
+        {
+            return 1; // Palindrome
+        }
+        else
+        {
+            return 0; // Not a palindrome
+        }
+    }
     // Function to calculate the square root
-    // int square_root(int num)
-    // {
-    //     int i;
-    //     for (i = 1; i <= num; i++)
-    //     {
-    //         if (i * i == num)
-    //         {
-    //             return i;
-    //         }
-    //     }
-    //     return 0;
-    // }
+    int square_root(int num)
+    {
+        int i;
+        for (i = 1; i <= num; i++)
+        {
+            if (i * i == num)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
     // Function to determine if a number is a perfect square
-    // int is_perfect_square(int num)
-    // {
-    //     int i;
-    //     for (i = 1; i <= num; i++)
-    //     {
-    //         if (i * i == num)
-    //         {
-    //             return 1; // Perfect square
-    //         }
-    //     }
-    //     return 0; // Not a perfect square
-    // }
+    int is_perfect_square(int num)
+    {
+        int i;
+        for (i = 1; i <= num; i++)
+        {
+            if (i * i == num)
+            {
+                return 1; // Perfect square
+            }
+        }
+        return 0; // Not a perfect square
+    }
 }
