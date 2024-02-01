c8c27d567d6e22dffc6a9a74f5b9022fcb6416af
Evon Troy Alexander <evont.alexander@gmail.com>
2024-02-01 09:05:32 -0500

    Commented out the sample code

--- Assignment1.c
+++ Assignment1.c
@@ -39,40 +39,45 @@ int main()
                 return 0; // Not a prime number
             }
         }
+        return 1; // Prime number
+    }
+
+    int main()
+    {
     }
 
     pid_t child_pid;
 
-    // Fork a new process
-    child_pid = fork();
+    // // Fork a new process
+    // child_pid = fork();
 
-    if (child_pid < 0)
-    {
-        perror("Fork failed");
-        exit(EXIT_FAILURE);
-    }
-    else if (child_pid == 0)
-    {
-        // This code will be executed by the child process
+    // if (child_pid < 0)
+    // {
+    //     perror("Fork failed");
+    //     exit(EXIT_FAILURE);
+    // }
+    // else if (child_pid == 0)
+    // {
+    //     // This code will be executed by the child process
 
-        // Execute a new program using execl
-        execl("/bin/ls", "ls", "-l", NULL);
+    //     // Execute a new program using execl
+    //     execl("/bin/ls", "ls", "-l", NULL);
 
-        // If execl fails, this code will be reached
-        perror("Execl failed");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        // This code will be executed by the parent process
+    //     // If execl fails, this code will be reached
+    //     perror("Execl failed");
+    //     exit(EXIT_FAILURE);
+    // }
+    // else
+    // {
+    //     // This code will be executed by the parent process
 
-        printf("Parent: Child process created with PID %d\n", child_pid);
+    //     printf("Parent: Child process created with PID %d\n", child_pid);
 
-        // Wait for the child process to finish
-        wait(NULL);
+    //     // Wait for the child process to finish
+    //     wait(NULL);
 
-        printf("Parent: Child process finished\n");
-    }
+    //     printf("Parent: Child process finished\n");
+    // }
 
-    return 0;
+    // return 0;
 }
