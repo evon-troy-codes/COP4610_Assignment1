a8ed6347e6f9ab894f6d15192e7d4fd59eb9c080
Evon Troy Alexander <evont.alexander@gmail.com>
2024-02-01 16:55:37 -0500

    Some refactoring. Added comments and removed some unnecessary code.

--- Assignment1.c
+++ Assignment1.c
@@ -1,4 +1,7 @@
-// Group Members: Samira Masri () & Evon Troy Alexander (1945955)
+//  Group Members: Samira Masri () & Evon Troy Alexander (1945955)
+//  This program simulates a parallel task execution scenario using process forking.
+//  The user will be prompted to enter a number between 1 and 5 (child processes to be created).
+//  Each child process will perform a unique task and the parent process will wait for all child processes to finish before it finishes.
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,77 +10,86 @@
 
 int main()
 {
+    // Variable declaration
+    int num;
 
-    // Function to determine the sum of the factors of a number
-    int factorial(int sum_factor)
-    {
-        int i, sum = 0;
-        for (i = 1; i <= sum_factor; i++)
-        {
-            if (sum_factor % i == 0)
-            {
-                sum += i;
-            }
-        }
-        return sum;
-    }
+    // Here we'll prompt the user to enter a number
+    printf("How many child processes would you like to create? Please enter a number between 1 and 5: ");
+    scanf("%d", &num);
 
-    // Function to determine if a number is prime
-    int is_prime_num(int num)
+    // If the number is < 1 or > 5, we'll prompt the user to enter a valid number
+    while (num < 1 || num > 5)
     {
-        // In the initial loop we'll check to see if the number is 1 or 2
-        if (num < 2)
-        {
-            return 0; // Not a prime number
-        }
-        for (int i = 2; i < num; i++)
-        {
-            if (num % i == 0)
-            {
-                return 0; // Not a prime number
-            }
-        }
-        return 1; // Prime number
+        printf("Invalid entry. Please enter a number between 1 and 5: ");
+        scanf("%d", &num);
     }
-
-    int main()
-    {
-        // Variable declarations
-        int sum_factor, num;
-    }
+    // User is notified of the number of child processes to be created
+    printf("Parent process (PID: %d) will create %d child processes per the users request.", getpid());
 
     pid_t child_pid;
 
-    // // Fork a new process
-    // child_pid = fork();
+    // Fork a new process
+    child_pid = fork();
 
-    // if (child_pid < 0)
+    if (child_pid < 0)
+    {
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    }
+    else if (child_pid == 0)
+    {
+        // This code will be executed by the child process
+
+        // Execute a new program using execl
+        execl("/bin/ls", "ls", "-l", NULL);
+
+        // If execl fails, this code will be reached
+        perror("Execl failed");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        // This code will be executed by the parent process
+
+        printf("Parent: Child process created with PID %d\n", child_pid);
+
+        // Wait for the child process to finish
+        wait(NULL);
+
+        printf("Parent: Child process finished\n");
+    }
+
+    return 0;
+
+    //         // Function to determine the sum of the factors of a number
+    // int factorial(int sum_factor)
     // {
-    //     perror("Fork failed");
-    //     exit(EXIT_FAILURE);
+    //     int i, sum = 0;
+    //     for (i = 1; i <= sum_factor; i++)
+    //     {
+    //         if (sum_factor % i == 0)
+    //         {
+    //             sum += i;
+    //         }
+    //     }
+    //     return sum;
     // }
-    // else if (child_pid == 0)
+
+    // // Function to determine if a number is prime
+    // int is_prime_num(int num)
     // {
-    //     // This code will be executed by the child process
-
-    //     // Execute a new program using execl
-    //     execl("/bin/ls", "ls", "-l", NULL);
-
-    //     // If execl fails, this code will be reached
-    //     perror("Execl failed");
-    //     exit(EXIT_FAILURE);
+    //     // In the initial loop we'll check to see if the number is 1 or 2
+    //     if (num < 2)
+    //     {
+    //         return 0; // Not a prime number
+    //     }
+    //     for (int i = 2; i < num; i++)
+    //     {
+    //         if (num % i == 0)
+    //         {
+    //             return 0; // Not a prime number
+    //         }
+    //     }
+    //     return 1; // Prime number
     // }
-    // else
-    // {
-    //     // This code will be executed by the parent process
-
-    //     printf("Parent: Child process created with PID %d\n", child_pid);
-
-    //     // Wait for the child process to finish
-    //     wait(NULL);
-
-    //     printf("Parent: Child process finished\n");
-    // }
-
-    // return 0;
 }
